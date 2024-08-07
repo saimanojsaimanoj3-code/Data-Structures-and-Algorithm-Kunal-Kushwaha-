2d492e46d6881b4686667214454ee10133944e22
Purushotham011 <Purushothamb011@gmail.com>
2024-08-07 10:12:39 +0530

    Added NQueenProblem

---
+++ Day-2/NQueenProblem.java
@@ -0,0 +1,85 @@
+/* Java program to solve N Queen Problem using backtracking */
+public class NQueenProblem {
+	final int N = 18;
+
+	void printSolution(int board[][])
+	{
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++)
+				System.out.print(" " + board[i][j]
+								+ " ");
+			System.out.println();
+		}
+	}
+
+	boolean isSafe(int board[][], int row, int col)
+	{
+		int i, j;
+
+		for (i = 0; i < col; i++)
+			if (board[row][i] == 1)
+				return false;
+
+		for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		for (i = row, j = col; j >= 0 && i < N; i++, j--)
+			if (board[i][j] == 1)
+				return false;
+
+		return true;
+	}
+
+	
+	boolean solveNQUtil(int board[][], int col)
+	{
+		
+		if (col >= N)
+			return true;
+
+		
+		for (int i = 0; i < N; i++) {
+			
+			if (isSafe(board, i, col)) {
+				/* Place this queen in board[i][col] */
+				board[i][col] = 1;
+
+				/* recur to place rest of the queens */
+				if (solveNQUtil(board, col + 1) == true)
+					return true;
+
+				
+				board[i][col] = 0; // BACKTRACK
+			}
+		}
+
+		
+		return false;
+	}
+
+	boolean solveNQ()
+	{
+		int board[][] = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				board[i][j] = 0;
+			}
+		}
+		
+
+		if (solveNQUtil(board, 0) == false) {
+			System.out.print("Solution does not exist");
+			return false;
+		}
+
+		printSolution(board);
+		return true;
+	}
+
+	public static void main(String args[])
+	{
+		NQueenProblem Queen = new NQueenProblem();
+		Queen.solveNQ();
+	}
+}
